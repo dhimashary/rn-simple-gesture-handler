591d49cd436500935e3b3259da710c4574d5551a
Ayu Sudi <ayusudi.abc@gmail.com>
2023-01-25 07:48:59 +0700

    Simple RN App with Gesture Hanlder Tap and Pinch

---
+++ App.js
@@ -0,0 +1,83 @@
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Image,
+  ImageBackground,
+  Alert,
+} from "react-native";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
+import backgroundImg from "./assets/bg.png";
+const characterImg = {
+  url: "https://static.wikia.nocookie.net/tamagotchi/images/e/e5/Mametchi_standing.png/revision/latest?cb=20220714103055",
+};
+
+export default function App() {
+  const singleTap = Gesture.Tap()
+    .maxDuration(250)
+    .onStart(() => {
+      Alert.alert("Single Tap");
+    });
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      Alert.alert("Double Tap", "Do you want to exit the game?", [
+        {
+          text: "Yes",
+          onPress: () => console.log("exit"),
+        },
+        {
+          text: "No",
+          onPress: () => console.log("stay"),
+          style: "cancel",
+        },
+      ]);
+    });
+
+  const pinch = Gesture.Pinch().onEnd(() => {
+    Alert.alert("Sakit tahuu");
+  });
+
+  const exlusiveGesture = Gesture.Exclusive(pinch, doubleTap, singleTap);
+
+  return (
+    <SafeAreaView style={styles.containerSafeArea}>
+      <ImageBackground source={backgroundImg} style={styles.background}>
+        <GestureDetector gesture={doubleTap}>
+          <Text style={styles.topText}>TamaGotchiApp</Text>
+        </GestureDetector>
+        <GestureDetector gesture={exlusiveGesture}>
+          <Image source={characterImg} style={styles.character} />
+        </GestureDetector>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerSafeArea: {
+    flex: 1,
+    backgroundColor: "#5C3526",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  character: {
+    width: 400,
+    height: 400,
+    opacity: 1,
+    resizeMode: "contain",
+  },
+  topText: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: "20",
+    color: "#42261c",
+    margin: 10,
+    position: "absolute",
+    top: 0,
+  },
+});
---
+++ README.md
@@ -0,0 +1,37 @@
+# LECTURE W2D2 PM
+
+## React Native : Styling & Gestrure Handler
+
+- [StyleSheet](https://reactnative.dev/docs/stylesheet)
+- [React Native Gesture Handler](https://docs.swmansion.com/react-native-gesture-handler/)
+  - Installation
+  - Implementation Test Cae
+    - Tap
+    - Pinch
+    - Composing Gesture  
+      Demo: combine both of gesture with Exclusive.
+
+# Tamagotchi App
+
+<img src="./demo.png" width="200" />
+
+![demo](./demo.gif)
+
+### Style Information
+
+- Color
+  - Background SafeArea : #5C3526
+  - Text Color :
+- Asset
+  - Character  
+    https://static.wikia.nocookie.net/tamagotchi/images/e/e5/Mametchi_standing.png/revision/latest?cb=20220714103055
+  - Background Image : Local file png `./assets/bg.png`
+
+## Other Mentionable Topic
+
+- SafeArea
+- Alert
+- Image
+- ImageBackground.
+- Source image from path and local file.
+- Style `object-fit` in RN is `resizeMode`
